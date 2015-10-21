dc918a4bf263343c20d9a7dad429dd9ee1e97db7
Louis Sloss <louis,sloss@gmail.com>
2015-10-21 14:06:23 -0700

    added large show

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,7 +1,7 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
 
-	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+	has_attached_file :image, styles: {large: "600x600", medium: "300x300>"}
 	# Validate content type
   	validates_attachment_content_type :image, content_type: /\Aimage/
   	# Validate filename
