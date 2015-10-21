02d10c9a45c5f84a236567b944eb680d219c3ada
Louis Sloss <louis,sloss@gmail.com>
2015-10-21 13:51:40 -0700

    Reorganized header, removed welcome message, changed to Winterest

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
 root "pins#index"
 get "about" => "pages#about" #creates about_path
-get "team" => "pages#team" #creates team_pash
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
